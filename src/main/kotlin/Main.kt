8138afa63333b78f535dbe8089f891df8232f6a9
TetrisLitHub <77713718+TetrisLitHub@users.noreply.github.com>
2022-04-07 19:23:43 -0500

    Server connection and project layout created

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,29 +2,58 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import java.io.PrintWriter
 import java.net.Socket
+import java.util.*
+import kotlin.system.exitProcess
 
 var name = "" ; var server = ""
+var socket: Socket? = null
+var inputStream: Scanner? = null
+var outputStream: PrintWriter? = null
 
 fun main() = runBlocking {
-    println("Kotlin Web Chat\ntype your name:")
-    name = readln()
-    println("type server name:")
-    server = readln()
+    println("Kotlin Web Chat")
 
-    println("connecting to server...")
-    val s: Socket = withContext(Dispatchers.IO) {
-        Socket(server, 9876)
+    while (name.isEmpty().or(name.isBlank())) {
+        println("enter username:")
+        name = readln()
     }
 
-    val input  = async { listenIncoming() }
-    val output = async { sendOutgoing()   }
+    while (server.isEmpty().or(server.isBlank())) {
+        println("enter server IP:")
+        server = readln()
+    }
+
+    println("connecting to server...")
+    try {
+        socket = withContext(Dispatchers.IO) {
+            Socket(server, 9876)
+        }
+        inputStream = Scanner(withContext(Dispatchers.IO) {
+            socket!!.getInputStream()
+        })
+        outputStream = PrintWriter(withContext(Dispatchers.IO) {
+            socket!!.getOutputStream()
+        })
+    } catch (e: Exception) {
+        e.printStackTrace()
+        System.err.println("\nUNABLE TO CONNECT TO SERVER. PRESS ANY KEY TO EXIT")
+        readln()
+        exitProcess(0)
+    }
+
+    outputStream!!.println(name) // send name to server first
+
+    val incoming = async { listenIncoming() }.start()
+    val outgoing = async { sendOutgoing()   }.start()
 }
 
 suspend fun listenIncoming() { while (true) {
-
+    // TODO: write listener
 }}
 
 suspend fun sendOutgoing() { while (true) {
     var dataToSend = readln()
+    // TODO: write sender
 }}
